1ace228eb7814798d6f752d4cd4767facb97a741
Aline Rabello <aline.rabello.jose@gmail.com>
2026-06-22 10:26:42 -0300

    chore(finance-chart): second push

--- src/app/components/finance-chart/finance-chart.ts
+++ src/app/components/finance-chart/finance-chart.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-finance-chart',
@@ -6,4 +7,17 @@ import { Component } from '@angular/core';
   templateUrl: './finance-chart.html',
   styleUrl: './finance-chart.scss',
 })
-export class FinanceChartComponent {}
+export class FinanceChartComponent implements OnInit {
+  private http = inject(HttpClient);
+  public dataFinance: any;
+
+  ngOnInit() {
+    this.http.get<any>('assets/finance-data.json').subscribe({
+      next: (res) => {
+        this.dataFinance = res;
+        console.log('Dados lidos com sucesso!', this.dataFinance);
+      },
+      error: (error) => console.error('Erro ao ler o arquivo JSON:', error)
+    });
+  }
+}
